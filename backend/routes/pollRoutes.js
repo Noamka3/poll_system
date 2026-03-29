a1026d658f973cccad6693f245e5d995239c1212
Noamka3 <Noamkadosh4444@gmail.com>
2026-03-29 20:47:26 +0300

    update Middleware

--- backend/routes/pollRoutes.js
+++ backend/routes/pollRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const pollController = require("../controllers/pollController");
+const limiter = require("../middleware/rateLimiter");
 
 router.get("/", pollController.getAllPolls);
-router.post("/", pollController.createPoll);
+router.post("/", limiter, pollController.createPoll);
 router.get("/:id/results", pollController.getPollResults); // ספציפי קודם!
 router.get("/:id", pollController.getPollById);            // דינמי אחרון
-router.post("/:id/vote", pollController.vote);
+router.post("/:id/vote", limiter, pollController.vote);
 
 module.exports = router;
